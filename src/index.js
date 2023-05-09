ea03c7dc07a80ebd9d7e6b26b7ffb057d107a074
Reimon Odakura <pwd2mmi2ig@yahoo.co.jp>
2023-05-09 22:46:03 +0000

    Update 4 files

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ const onClickAdd = () => {
   // 入力値取得とクリア
   const input = document.getElementById("input").value;
   document.getElementById("input").value = "";
+  addIncomplateArea(input);
+};
 
+// 未完了エリアからの削除
+const deleteFromIncomplateArea = (targetElement) => {
+  document.getElementById("incomplate-ul").removeChild(targetElement);
+};
+
+// 未完了エリアへ追加
+const addIncomplateArea = (text) => {
   //div作成
   const div = document.createElement("div");
   div.className = "one_line";
 
   //li作成
   const li = document.createElement("li");
-  li.innerText = input;
+  li.innerText = text;
 
   // ■ 完了ボタン作成
   const complateButton = document.createElement("button");
@@ -20,16 +29,17 @@ const onClickAdd = () => {
   complateButton.addEventListener("click", () => {
     const moveTarget = complateButton.parentNode;
     // 未完了エリアからの削除
-    deleteFromIncomplateArea(moveTarget);
+    document.getElementById("incomplate-ul").removeChild(moveTarget);
+
     const children = moveTarget.childNodes;
     moveTarget.removeChild(children[1]);
 
     children[1].textContent = "戻る";
     children[1].addEventListener("click", () => {
       // 戻るボタン押下時の処理
-      document
-        .getElementById("complate-ul")
-        .removeChild(children[1].parentNode);
+      document.getElementById("complate-ul").removeChild(moveTarget);
+      //console.log(children[0].textContent);
+      addIncomplateArea(children[0].textContent);
     });
 
     moveTarget.appendChild(children[1]);
@@ -42,7 +52,9 @@ const onClickAdd = () => {
   // 削除ボタン押下時処理
   deleteButton.addEventListener("click", () => {
     // 未完了エリアからの削除
-    deleteFromIncomplateArea(deleteButton.parentNode);
+    document
+      .getElementById("incomplate-ul")
+      .removeChild(deleteButton.parentNode);
   });
 
   //未作成リストに設定
@@ -50,11 +62,6 @@ const onClickAdd = () => {
   div.appendChild(complateButton);
   div.appendChild(deleteButton);
   document.getElementById("incomplate-ul").appendChild(div);
-};
-
-// 未完了エリアからの削除
-const deleteFromIncomplateArea = (targetElement) => {
-  document.getElementById("incomplate-ul").removeChild(targetElement);
 };
 
 // 追加ボタン押下時処理
